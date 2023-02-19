32db3f57dd227d51acfc00c69c30e566dd5348f9
Rafael <rafael.2018.ads@gmail.com>
2023-02-19 19:10:33 -0300

    Projeto iniciado

---
+++ src/database/migrations/20230219205039-aluno.js
@@ -0,0 +1,55 @@
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.createTable('aluno', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('aluno');
+     */
+  }
+};
---
+++ src/routes/home.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+const router = new Router();
+
+import homeController from '../controllers/HomeController';
+
+router.get('/', homeController.index);
+
+
+export default router;
